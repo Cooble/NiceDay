7a39d9974dffcf8666d9c152d3ea5105f157fdb1
Cobble <minekoza@gmail.com>
2019-05-22 08:38:41 +0200

    Create Background.h

---
+++ NiceDay/src/world/Background.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "ndpch.h" //todo add glm to precompiled header
+#include "buffers/VertexArray.h"
+#include "buffers/VertexBuffer.h"
+
+
+class Background
+{
+  private:
+    //std::vector<glm::mat4>
+
+
+}
